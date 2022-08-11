ca9652e37b1f6acf7dd81e9681bfa4c967079428
denismezhenin <dzianismiazhenin@gmail.com>
2022-08-11 16:00:25 +0300

    init: start weather widget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -58,26 +58,66 @@ window.addEventListener('load', getLocalStorage)
 // END greetings
 
 // Start slider
+const buttonNext = document.querySelector('.slide-next')
+const buttonPrev = document.querySelector('.slide-prev')
 
-
-let random 
+let randomNum 
 function getRandomNumber() {
-  random = (Math.round(Math.random() * (20 - 1) + 1)).toString().padStart(2, '0')
-  return random 
+  randomNum = ((Math.round(Math.random() * (20 - 1) + 1)).toString().padStart(2, '0'))
+  return randomNum 
  }
  getRandomNumber()
 
  function setBg() {
-  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/denismezhenin/stage1-tasks/assets/images/${getTimeofDay(hours)}/${random}.jpg')`
- 
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/denismezhenin/stage1-tasks/assets/images/${getTimeofDay(hours)}/${randomNum}.jpg`;
+  img.onload = () => {
+    // console.log(img.src)
+    document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/denismezhenin/stage1-tasks/assets/images/${getTimeofDay(hours)}/${randomNum}.jpg')`
+  };
+  
 }
  setBg()
 
+ function getSlideNext() {
+  randomNum = (Number(randomNum) + 1).toString().padStart(2, '0')
+  console.log()
+  if (randomNum == '21') {
+    randomNum = '01'
+  }
+  setBg()
+ }
 
+ function getSlidePrev() {
+  randomNum = (Number(randomNum) - 1).toString().padStart(2, '0')
+  if (randomNum == 00) {
+    randomNum = '20'
+  }
+  setBg()
+ }
 
-
-
-
- 
+ buttonNext.addEventListener('click', getSlideNext)
+ buttonPrev.addEventListener('click', getSlidePrev)
 
 // END slider
+
+// Weather widget start
+// https://api.openweathermap.org/data/2.5/weather?q=minsk&lang=en&appid=c4549f43d9c0e684a43b8474124bd5af&units=metric
+const weatherIcon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temperature')
+const weatherDescription = document.querySelector('.weather-description')
+
+async function getWeather(url) {
+
+  const res = await fetch(url);
+  const data = await res.json()
+  console.log(data.weather[0].id, data.weather[0].description, data.main.temp)
+  weatherIcon.className = 'weather-icon owf'
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+getWeather(`https://api.openweathermap.org/data/2.5/weather?q=minsk&lang=en&appid=c4549f43d9c0e684a43b8474124bd5af&units=metric`)
+
+
+// Weather widget end
